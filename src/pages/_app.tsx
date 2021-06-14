5658628d75ffb88ac7ac0f16791c34ee3852956d
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-15 02:49:09 +0545

    Refactor: Setup and run eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import React from 'react';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;

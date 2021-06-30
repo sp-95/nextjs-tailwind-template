c945c94afad2104a2d25fda8d144c3779f70fef2
Shashanka Prajapati <shashankap95@gmail.com>
2021-06-30 17:23:25 +0545

    Style: Sticky header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header: React.FunctionComponent = () => {
-  return <header />
+  return <header className="sticky top-0 shadow-xl z-50" />
 }
 
 export default Header

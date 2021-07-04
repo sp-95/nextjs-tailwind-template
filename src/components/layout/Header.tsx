2efc877504eaecb6ca5a41e5b62e6d06bdbf7641
Shashanka Prajapati <shashankap95@gmail.com>
2021-07-04 11:52:35 +0545

    Refactor: Change style folder structure

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header: React.FunctionComponent = () => {
-  return <header className="sticky top-0 shadow-xl z-50" />
+  return <header />
 }
 
 export default Header
